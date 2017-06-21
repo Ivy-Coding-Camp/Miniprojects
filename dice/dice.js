c194d6760cf466bf485602940b8465fdf7c406ed
Ajusa Jain <arhamjain@gmail.com>
2017-06-21 15:25:51 -0400

    added comments for dice roller

--- dice/dice.js
+++ dice/dice.js
@@ -1,1 +1,8 @@
-alert("The result of the die roll was " + (Math.floor(Math.random()*6) + 1))
+/*
+The goal is to display a random number between 1 and 6.
+*/
+roll = Math.floor(Math.random()*6) + 1 
+//We get a number between 0 and 1 (but not including 1!). We then multiply that by 6, so it goes from 0 and 5.999... 
+//We then truncate the number past the decimal (0 to 5) and add 1 (1 to 6)
+alert("The result of the die roll was " + roll) //we combine the string and the number we had generated
+// We call the javascript alert function to display the result
--- dice/dice.py
+++ dice/dice.py
@@ -1,3 +1,4 @@
-from random import randint
+from random import randint #we import the randint function from the library random
 print("The result was " + str(randint(1,6)))
-
+#We print out the result, and we combine a string and an int. To combine these, we cast (transform) the int to a string using the 'str' function
+#randint returns an integer including 1 and 6, and between them. Remember, ints don't have decimal places.
