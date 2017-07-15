cbd6077687b63a488335ce1e8383d4e937bf07c7
Antigluon <optionaloctave@gmail.com>
2017-07-15 14:35:14 -0400

    Typo

--- magic8ball/magic8ball.py
+++ magic8ball/magic8ball.py
@@ -6,7 +6,7 @@ answers = ["It is certain", "Without a doubt", "Yes, definitely",
            "Concentrate and ask again", "Don't count on it", "My reply is no",
            "My sources say no", "Outlook not so good", "Very doubtful"] #here, we declare a list of strings. 
 ans = '!' #we give ans a value so that the while loop will execute. 
-while ans: #This will keep on looping as long as ans is not black. If a variable stores nothing, it returns false when checked
+while ans: #This will keep on looping as long as ans is not blank. If a variable stores nothing, it returns false when checked
     ans = input("Ask the magic 8 ball a question. (Press enter to leave): \n") 
     #The reason we store the input is so the user can exit the program by passing in nothing for ans
     print(random.choice(answers)) #the random library lets us draw a random string from a list. We then print it
